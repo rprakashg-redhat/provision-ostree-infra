4745d1e774a040b74a8aa593cdd369db9ccc627e
Ram <rgopinat@redhat.com>
2024-08-08 17:19:06 -0700

    Updated init script

--- init.sh
+++ init.sh
@@ -2,6 +2,7 @@
 
 sudo dnf -y install ansible-core
 sudo dnf -y install git-all
+sudo dnf -y install podman
 
 ansible-galaxy collection install -f git+https://github.com/redhat-cop/infra.osbuild --upgrade
 ansible-galaxy collection install -f containers.podman --upgrade
