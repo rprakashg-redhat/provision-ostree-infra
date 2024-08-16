bb2b0e91436a801658c2f048fb6a67fcf267dc26
Ram <rgopinat@redhat.com>
2024-08-16 16:34:51 -0700

    Upgrading providers and added s3 bucket for elb logs

--- elb.tf
+++ elb.tf
@@ -4,6 +4,9 @@ module "elb" {
 
     name = "${var.stack}-elb"
     subnets = module.vpc.public_subnets
+    access_logs = {
+        bucket = module.elblogs.s3_bucket_id
+    }
     health_check = {
         target              = "TCP:80"
         interval            = 30
@@ -13,17 +16,21 @@ module "elb" {
     }
     listener = [
         {
-            instance_port     = "9090"
+            instance_port     = "443"
             instance_protocol = "HTTPS"
             lb_port           = "443"
             lb_protocol       = "HTTPS"
             ssl_certificate_id  = aws_acm_certificate.cert.id
         },
+        {
+            instance_port     = "80"
+            instance_protocol = "HTTP"
+            lb_port           = "80"
+            lb_protocol       = "HTTP"
+        },
     ]
     security_groups = [module.public_subnet_sg.security_group_id]
-
     # ELB attachments
     number_of_instances = 1
     instances           = module.ec2.id
-
 }
--- versions.tf
+++ versions.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
      aws = {
       source  = "hashicorp/aws"
-      version = "5.49.0"
+      version = "5.63.0"
     }
     acme = {
       source = "vancluever/acme"
-      version = "2.21.0"
+      version = "2.25.0"
     }
   }
 }
