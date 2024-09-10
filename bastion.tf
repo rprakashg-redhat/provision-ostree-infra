048b6d035959170af23bea4f971ffc10b27b9e04
Ram <rgopinat@redhat.com>
2024-09-10 12:27:08 -0700

    Removed instant profile scripts

--- bastion.tf
+++ bastion.tf
@@ -14,8 +14,6 @@ module "bastion" {
   ipv6_addresses = null
   private_ips = ["10.0.3.141"]
   
-  iam_instance_profile = aws_iam_instance_profile.bastion_instance_profile.name
-
   root_block_device = [
     {
       volume_type = "gp2"
@@ -37,54 +35,3 @@ module "bastion" {
   tags = local.tags
 }
 
-# IAM role for bastion node with SSM permissions
-resource "aws_iam_role" "bastion_instance_role" {
-  name = "BastionInstanceRole"
-
-  assume_role_policy = <<EOF
-    {
-      "Version": "2012-10-17",
-      "Statement": [
-        {
-          "Action": "sts:AssumeRole",
-          "Principal": {
-            "Service": "ec2.amazonaws.com"
-          },
-          "Effect": "Allow",
-          "Sid": ""
-        }
-      ]
-    }
-  EOF
-}
-
-# Bastion instance role policy
-resource "aws_iam_role_policy" "bastion_instance_policy" {
-  name = "BastionInstancePolicy"
-  role = aws_iam_role.bastion_instance_role.id
-
-  policy = <<EOF
-    {
-      "Version": "2012-10-17",
-      "Statement": [
-        {
-          "Effect": "Allow",
-          "Action": [
-            "s3:GetObject",
-            "s3:ListBucket",
-            "ssm:SendCommand",
-            "ssm:ListCommands",
-            "ssm:ListCommandInvocations",
-            "ssm:GetCommandInvocation"
-          ],
-          "Resource": "*"
-        }
-      ]
-    }
-  EOF
-}
-
-resource "aws_iam_instance_profile" "bastion_instance_profile" {
-  name = "BastionInstanceProfile"
-  role = aws_iam_role.bastion_instance_role.name
-}
